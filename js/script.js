bfbc539e6e870c4e3eae7a182aa1465ce560a6cc
Arkadiusz Piątek <piatek.arkadiusz@gmail.com>
2023-03-09 20:36:07 +0100

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -8,10 +8,11 @@ document.getElementById('test-button').addEventListener('click', function () {
 */
 
 const titleClickHandler = function (event) {
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -21,13 +22,16 @@ const titleClickHandler = function (event) {
 
   /* add class 'active' to the clicked link */
 
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
-
-  /* remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
