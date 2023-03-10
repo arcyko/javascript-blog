ee5f3d5d0afc1f213845b064fb820de3804ff1d4
Arkadiusz Piątek <piatek.arkadiusz@gmail.com>
2023-03-10 19:20:18 +0100

    Add other functions of the left column

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ document.getElementById('test-button').addEventListener('click', function () {
 */
 
 const titleClickHandler = function (event) {
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
@@ -20,7 +21,7 @@ const titleClickHandler = function (event) {
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
@@ -35,9 +36,17 @@ const titleClickHandler = function (event) {
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 };
 
 const links = document.querySelectorAll('.titles a');
